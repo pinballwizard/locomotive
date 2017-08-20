337d40e19f0e29e865cc832188aea4fce6ba5aae
pinballwizard <menstenebris@gmail.com>
2017-08-20 19:44:42 +0300

    add readme

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # locomotive
-test work for job in locomotive tehnologies
+Test work for job in locomotive tehnologies
+Тестовая работа для ООО ЛокоТех
 
-pip3 install Django, openpyxl, scipy, pandas,
+pip3 install Django, xlrd, pandas
+git clone https://github.com/pinballwizard/locomotive
+cd locomotive
+python3 manage.py runserver
+
+В браузере
+
+http://localhost:8000 - Основная страница
+http://localhost:8000/admin - Интерфейс администратора
+http://localhost:8000/drop - отчистить базу
+http://localhost:8000/reload - загрузить данные из файла
--- main/admin.py
+++ main/admin.py
@@ -6,14 +6,28 @@ AdminSite.site_header = 'Локомотивные технологии'
 AdminSite.site_title = 'Локомотивные технологии'
 
 
+class LocomotiveInLine(admin.TabularInline):
+    model = Locomotive
+
+
+class MileageInLine(admin.TabularInline):
+    model = Mileage
+
+
 @admin.register(Branch)
 class BranchAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    inlines = [
+        LocomotiveInLine,
+    ]
 
 
 @admin.register(Locomotive)
 class LocomotiveAdmin(admin.ModelAdmin):
     list_display = ('series', 'branch', 'rate')
+    inlines = [
+        MileageInLine,
+    ]
 
 
 @admin.register(Mileage)
